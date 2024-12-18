4a90f30d83fc8c204eeed5ba3f8d29c1abd344ca
saravananbs <bssaravanan247@gmail.com>
2024-12-18 18:05:18 +0530

    fixing deployment errors

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load("saravananbs.jpg");
+const jeffTexture = new THREE.TextureLoader().load("/saravananbs.jpg");
 
 const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
 
